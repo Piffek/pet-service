64d765e7972216611d9721fe75150aba7acc45f1
Piwko Patryk <patryk.piwko@god.dev>
2024-04-21 23:03:22 +0200

    init

---
+++ src/main/java/com/seniority/petservice/PetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.seniority.petservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seniority/petservice/PetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seniority.petservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
