176a675131377e6124fcdcf010c1066ef3be2c3a
Piwko Patryk <patryk.piwko@god.dev>
2024-05-13 23:52:26 +0200

    entrypoint health enabled

--- src/main/java/com/seniority/petservice/PetServiceApplication.java
+++ src/main/java/com/seniority/petservice/PetServiceApplication.java
@@ -1,11 +1,9 @@
 package com.seniority.petservice;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRabbit
 public class PetServiceApplication {
 
 	public static void main(String[] args) {
