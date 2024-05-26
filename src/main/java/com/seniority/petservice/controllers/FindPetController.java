db649e8a551783aba8a8b81f55f13ef943996b8e
Piwko Patryk <patryk.piwko@god.dev>
2024-05-26 22:40:20 +0200

    divide properties to local and docker

--- src/main/java/com/seniority/petservice/controllers/FindPetController.java
+++ src/main/java/com/seniority/petservice/controllers/FindPetController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("dpc")
+@RequestMapping("/api/pet/find")
 @RequiredArgsConstructor
 public class FindPetController {
 
