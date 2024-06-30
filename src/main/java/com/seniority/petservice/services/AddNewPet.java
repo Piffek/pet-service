aab0f06d0953d61b44bfa78595e08c45329e8b3a
Piwko Patryk <patryk.piwko@god.dev>
2024-06-30 20:40:27 +0200

    implement cache for show pets in shelter

--- src/main/java/com/seniority/petservice/services/AddNewPet.java
+++ src/main/java/com/seniority/petservice/services/AddNewPet.java
@@ -1,9 +1,11 @@
 package com.seniority.petservice.services;
 
+import com.seniority.petservice.config.Cache;
 import com.seniority.petservice.messagebroker.command.AddPetCommand;
 import com.seniority.petservice.models.Pet;
 import com.seniority.petservice.repositories.PetRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,7 @@ public class AddNewPet {
 
     private final PetRepository petRepository;
 
+    @CacheEvict(value = Cache.PETS, key = "#addPetCommand.shelterId")
     public void create(AddPetCommand addPetCommand) {
         var pet = convert(addPetCommand);
         petRepository.save(pet);
